e1fd6ba992b6feb0343d12c1f0f237d2bb2f0221
Juan Lira <juancassiano@hotmail.com>
2024-08-06 14:00:47 -0300

    build: core project file

--- cmd/tools/terndotenv/main.go
+++ cmd/tools/terndotenv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/joho/godotenv"
@@ -17,9 +18,14 @@ func main() {
 		"--migrations",
 		"./internal/store/pgstore/migrations",
 		"--config",
-		"./internal/store/pgstore/migrations/tern.conf")
-
-	if err := cmd.Run(); err != nil {
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Command execution failed with error: %v\n", err)
+		fmt.Printf("Output: %s\n", string(output))
 		panic(err)
 	}
+
+	fmt.Printf("Command executed successfully: %s\n", string(output))
 }
